9812ab9479c69427d5d78ea7fe8ef0618569c1a7
Vinipcsl <111318695+Vinipcsl@users.noreply.github.com>
2023-03-02 08:54:20 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
 
 Iphone ios = new Iphone("62 9 82606375","Iphone 8 Plus","8762468576521357",64);
 Nokia nokia = new Nokia("62 9 82606375","Windows Phone","1135486524961321",32);
